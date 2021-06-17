3fd13ac842fcc1c802b6b35e4b9eea5bc5b60733
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-06-16 20:15:01 -0500

    finished studio

--- studio/Rocket.ts
+++ studio/Rocket.ts
@@ -1,12 +1,53 @@
+import { Cargo } from './Cargo';
+import { Astronaut } from './Astronaut';
 import { Payload } from './Payload';
 
-export class Rocket implements Payload {
+
+export class Rocket {
     name: string;
     totalCapacityKg: number;
-    cargoItems: [];
-    astronauts: [];
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
+
     constructor(name: string, totalCapacityKg: number) {
         this.name = name;
         this.totalCapacityKg = totalCapacityKg;
     }
+
+    sumMass( items: Payload[] ): number {
+        let sum: number = 0;
+        for(let i: number = 0; i < items.length; i++) {
+            sum += items[i].massKg;
+        }
+        return sum;
+    }
+
+    currentMassKg(): number {
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems)
+    }
+
+    canAdd(item: Payload): boolean {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+        return false;
+    }
+
+    addCargo(cargo: Cargo): boolean {
+        if(this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        return false;
+    }
+
+    addAstronaut(astronaut: Astronaut): boolean {
+        if(this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        return false;
+    }
+
+
 }
