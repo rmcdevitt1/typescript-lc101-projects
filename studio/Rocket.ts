41036e8a8c6c1e8c4ec7a720b25c4013c13b0633
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-06-16 19:47:49 -0500

    added implementations

--- studio/Rocket.ts
+++ studio/Rocket.ts
@@ -1,8 +1,12 @@
 import { Payload } from './Payload';
 
-export class Rocket {
+export class Rocket implements Payload {
     name: string;
     totalCapacityKg: number;
     cargoItems: [];
     astronauts: [];
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
 }
